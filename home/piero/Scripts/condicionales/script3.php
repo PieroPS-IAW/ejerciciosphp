3e12314240e848fbf80bec167ba6da1911057203
PieroIAW <aa21.plcastillo43@iespabloserrano.com>
2024-10-24 20:35:22 +0200

    condicionalfinal

--- home/piero/Scripts/condicionales/script3.php
+++ home/piero/Scripts/condicionales/script3.php
@@ -4,21 +4,24 @@
 $n1 = rand(0,50);
 $n2 = rand(0,50);
 $n3 = rand(0,50);
+/*$n1 = 6;
+$n2 = 7;
+$n3 =6;*/
 echo "$n1,$n2,$n3 <br>";
-if ($n1 > $n2 && $n1 > $n3) {
-    if ($n2 > $n3) {
+if ($n1 >= $n2 && $n1 >= $n3) {
+    if ($n2 >= $n3) {
         echo "$n1 > $n2 > $n3";
     } else {
         echo "$n1 > $n3 > $n2";
     }
-} elseif ($n2 > $n1 && $n2 > $n3) {
-    if ($n1 > $n3) {
+} elseif ($n2 >= $n1 && $n2 >= $n3) {
+    if ($n1 >= $n3) {
         echo "$n2 > $n1 > $n3";
     } else {
         echo "$n2 > $n3 > $n1";
     }
 } else {
-    if ($n1 > $n2) {
+    if ($n1 >= $n2) {
         echo "$n3 > $n1 > $n2";
     } else {
         echo "$n3 > $n2 > $n1";
--- home/piero/Scripts/condicionales/script5.php
+++ home/piero/Scripts/condicionales/script5.php
@@ -1,3 +1,34 @@
 <?php
 //EJERCICIO 5
 //Crea un programa con una variable booleana $lluvia y una variable $temperatura a las que darás valor en el código. El programa debe dar los siguientes mensajes. 
+$lluvia = true;  // true si está lloviendo, false si no
+$temperatura = rand(-20, 50);
+
+// Corrección: Usar "==" para comparar valores en vez de "="
+if ($lluvia == true) {
+    echo "Hoy llueve y la temperatura es: $temperatura<br>";
+} elseif ($lluvia == false) {
+    echo "Hoy no llueve y la temperatura es: $temperatura<br>";
+}
+
+echo "CONSEJO: ";
+
+// Consejos basados en si llueve o no y la temperatura
+if ($lluvia && $temperatura >= -10 && $temperatura <= 40) {
+    echo "Hoy llueve y debes quedarte en casa.";
+} elseif (!$lluvia) {
+    if ($temperatura > 20) {
+        echo "La temperatura es $temperatura y no llueve. ¡A la playa!";
+    } elseif ($temperatura >= -10 && $temperatura <= 5) {
+        echo "La temperatura es $temperatura y no llueve. ¡A esquiar!";
+    } elseif ($temperatura > 5 && $temperatura <= 20) {
+        echo "La temperatura es $temperatura y no llueve. De paseo.";
+    }
+}
+
+// Comprobar si la temperatura es extrema
+if ($temperatura < -10 || $temperatura > 40) {
+    echo "La temperatura es $temperatura y es una temperatura extrema.";
+}
+?>
+
