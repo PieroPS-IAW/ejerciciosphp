33a5bbfc9ab8179186157a72090eb5e1a97cf50f
PieroIAW <aa21.plcastillo43@iespabloserrano.com>
2024-11-07 20:12:12 +0100

    loops terminado

--- home/piero/Scripts/loops/script6.php
+++ home/piero/Scripts/loops/script6.php
@@ -1,4 +1,10 @@
 <?php
 //Crea un programa con una variable $n1 a la que daremos valor en el código. El programa mostrará todos los divisores de ese número.
 $n1 = rand(1,100);
-
+echo "Los divisores de $n1 son: ";
+for ($i = 1; $i <= $n1; $i++) {
+    if ($n1 % $i == 0) {
+        echo "$i ";  // Mostrar el divisor
+    }
+}
+?>
